64d3128861a2a30028d99e4b7e9a655f5dc23803
Pedro Diaz Jr <Diaz850@gmail.com>
2023-10-31 19:45:20 -0500

    reformatted JS

--- app.js
+++ app.js
@@ -10,39 +10,39 @@ const seattle = {
     avgCookiesPerCustomer: 6.3,
     cookiesSoldPerHour: [],
     totalCookiesSold: 0,
-  
+
     // Function to generate random customers and calculate cookies sold per hour
     simulateHourlySales: function () {
-      for (let hour = 6; hour <= 19; hour++) { // 6am to 7pm
-        let customers = Math.floor(Math.random() * (this.maxCustomersPerHour - this.minCustomersPerHour + 1)) + this.minCustomersPerHour;
-        let cookies = Math.round(customers * this.avgCookiesPerCustomer);
-        this.cookiesSoldPerHour.push(`${hour}am: ${cookies} cookies`);
-        this.totalCookiesSold += cookies;
-      }
-      this.displaySalesData();
+        for (let hour = 6; hour <= 19; hour++) { // 6am to 7pm
+            let customers = Math.floor(Math.random() * (this.maxCustomersPerHour - this.minCustomersPerHour + 1)) + this.minCustomersPerHour;
+            let cookies = Math.round(customers * this.avgCookiesPerCustomer);
+            this.cookiesSoldPerHour.push(`${hour}am: ${cookies} cookies`);
+            this.totalCookiesSold += cookies;
+        }
+        this.displaySalesData();
     },
-  
+
     // Function to display sales data in the browser
     displaySalesData: function () {
-      let salesList = document.getElementById('sales-list');
-      let locationTitle = document.createElement('h2');
-      locationTitle.textContent = this.name;
-      salesList.appendChild(locationTitle);
-  
-      let hourlyList = document.createElement('ul');
-      for (let i = 0; i < this.cookiesSoldPerHour.length; i++) {
-       let listItem = document.createElement('li');
-        listItem.textContent = this.cookiesSoldPerHour[i];
-        hourlyList.appendChild(listItem);
-      }
-  
-      let totalListItem = document.createElement('li');
-      totalListItem.textContent = `Total: ${this.totalCookiesSold} cookies`;
-      hourlyList.appendChild(totalListItem);
-  
-      salesList.appendChild(hourlyList);
+        let salesList = document.getElementById('sales-list');
+        let locationTitle = document.createElement('h2');
+        locationTitle.textContent = this.name;
+        salesList.appendChild(locationTitle);
+
+        let hourlyList = document.createElement('ul');
+        for (let i = 0; i < this.cookiesSoldPerHour.length; i++) {
+            let listItem = document.createElement('li');
+            listItem.textContent = this.cookiesSoldPerHour[i];
+            hourlyList.appendChild(listItem);
+        }
+
+        let totalListItem = document.createElement('li');
+        totalListItem.textContent = `Total: ${this.totalCookiesSold} cookies`;
+        hourlyList.appendChild(totalListItem);
+
+        salesList.appendChild(hourlyList);
     }
-  };
-  
-  // Call the simulateHourlySales method to generate and display sales data
-  seattle.simulateHourlySales();
+};
+
+// Call the simulateHourlySales method to generate and display sales data
+seattle.simulateHourlySales();
